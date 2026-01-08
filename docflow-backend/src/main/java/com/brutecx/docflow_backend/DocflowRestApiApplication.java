a33c6267a611407ef6782336543613f6719edfee
MihaiS-git <ingsuciumihai@gmail.com>
2026-01-08 19:30:04 +0200

    first commit

---
+++ docflow-backend/src/main/java/com/brutecx/docflow_backend/DocflowRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.brutecx.docflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocflowRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocflowRestApiApplication.class, args);
+	}
+
+}
